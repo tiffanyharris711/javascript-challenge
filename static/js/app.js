76c7a8effce21d0fc99adf874ff6dcc9063c94c1
tiffanyharris711 <tiffanyaharris711@gmail.com>
2021-04-12 20:48:44 -0500

    finished level 2 with date and city filter

--- static/js/app.js
+++ static/js/app.js
@@ -38,8 +38,23 @@ function runEnter() {
   d3.event.preventDefault();
   var inputElement = d3.select("#datetime");
   var inputValue = inputElement.property("value");
-  var filteredData = tableData.filter(inputDate => inputDate.datetime === inputValue);
   
+  var inputCityElement = d3.select("#city");
+  var inputCityValue = inputCityElement.property("value");
+
+  //use this one when city and date are neither blank
+  if (inputCityValue.length !== 0 && inputValue.length !== 0) {
+        var filteredData = tableData.filter(tableData => tableData.datetime === inputValue && tableData.city === inputCityValue);
+        }
+  //use this one when city is blank and date is not      
+  else if (inputCityValue.length === 0 && inputValue.length !== 0) {
+        var filteredData = tableData.filter(inputDate => inputDate.datetime === inputValue);
+        }
+  //use this one when date is blank and city is not
+  else if (inputCityValue.length !== 0 && inputValue.length === 0) {
+        var filteredData = tableData.filter(inputCity => inputCity.city === inputCityValue);
+        }
+
   // Clear table body so it can be reloaded with filtered data
   tbody.html("");
 
@@ -52,8 +67,10 @@ function runEnter() {
 function reset() {
   // Prevent the page from refreshing
   d3.event.preventDefault();
-  var inputElement = d3.select("#datetime");
-  var inputValue = inputElement.property("value");
+
+  // clear the input value
+  d3.select("#datetime").node().value = "";
+  d3.select("#city").node().value = "";
 
   // Clear table body so it can be reloaded with filtered data
   tbody.html("");
